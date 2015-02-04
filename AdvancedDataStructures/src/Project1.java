c24909b6e42ea4990fd51bb1cc826647c62bd66b
Kevin Ku <kkuo12887@gmail.com>
2015-02-04 17:02:08 -0500

    Setting up Project1.java.

--- AdvancedDataStructures/src/Project1.java
+++ AdvancedDataStructures/src/Project1.java
@@ -1,9 +1,5 @@
 /**
- * 
- */
-
-/**
- * @author J14688
+ * @author Kevin Kuo
  *
  */
 public class Project1 {
@@ -16,4 +12,25 @@ public class Project1 {
 
 	}
 
+}
+
+/**
+ * Reads data from text file and stores into an ArrayList
+ * 
+ * @param args
+ */
+class Driver {
+	
+	public static void readFile(){
+		System.out.println("Enter the name of the file in this directory that you would like to read:\n");
+	}
+	
+	
+}
+
+/**
+ * @param args
+ */
+class AssignScore {
+	
 }
