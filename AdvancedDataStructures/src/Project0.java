dff5bd4c6b1901c29664cc928d2de985f220d39e
Kevin Ku <kkuo12887@gmail.com>
2015-02-04 00:01:48 -0500

    Initial Commit

---
+++ AdvancedDataStructures/src/Project0.java
@@ -0,0 +1,43 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author Kevin Kuo
+ * 
+ *
+ */
+public class Project0 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("output.txt", "UTF-8");
+			
+			//initial version
+			System.out.println("Hello Towson!\n");
+			writer.println("Hello Towson!\n");
+			
+			//new version addition
+			System.out.println("How are you today?");
+			writer.println("How are you today?");
+			
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+	}
+
+}
---
+++ AdvancedDataStructures/src/Project1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author J14688
+ *
+ */
+public class Project1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
