0b030ba8069b3848fe86948b34326d331dad2686
Kevin Ku <kkuo12887@gmail.com>
2015-02-11 20:18:35 -0500

    Read from File complete.

--- AdvancedDataStructures/src/Project1.java
+++ AdvancedDataStructures/src/Project1.java
@@ -1,14 +1,64 @@
+import java.io.*;
+import java.util.*;
+
 /**
  * @author Kevin Kuo
  *
  */
 public class Project1 {
 
+
+	
+	static List<Integer> intAl = new ArrayList<Integer>();
+	
+	private int scoreSum = 0;
+		
+	/**
+	 * @return the scoreSum
+	 */
+	public int getScoreSum() {
+		return scoreSum;
+	}
+
+	/**
+	 * @param scoreSum the scoreSum to set
+	 */
+	public void setScoreSum(int scoreSum) {
+		this.scoreSum = scoreSum + this.scoreSum;
+	}
+
 	/**
 	 * @param args
-	 */
+	 */	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Driver driv = new Driver();
+		
+		Driver.readFile(intAl);
+		
+		printArrayList();
+		
+
+	}
+	
+	/**
+	 * Prints values in ArrayList.
+	 * 
+	 * @param args
+	 */
+	public static void printArrayList(){
+		
+		System.out.println("\nPrinting contents of ArrayList:\n");
+		
+		Iterator<Integer> it = intAl.iterator();
+		while (it.hasNext()){
+
+			System.out.println(it.next().toString());
+			
+		}
+		
+
 
 	}
 
@@ -21,8 +71,45 @@ public class Project1 {
  */
 class Driver {
 	
-	public static void readFile(){
-		System.out.println("Enter the name of the file in this directory that you would like to read:\n");
+	public static void readFile(List<Integer> intAl){
+	
+		BufferedReader br;
+		String line;
+		StringTokenizer st;
+		
+		int sum = 0;
+		
+		try {
+			
+			 System.out.println("\nCurrent working directory: " + System.getProperty("user.dir") + File.separator + "input.txt");
+			 br = new BufferedReader(new FileReader(System.getProperty("user.dir") + File.separator + "input.txt"));
+
+			 line = br.readLine();
+			 
+			 while (line != null){
+				 
+				 st = new StringTokenizer(line);
+				 
+				 while (st.hasMoreTokens()){
+					 
+					 int tempInt = Integer.parseInt(st.nextToken());
+					 intAl.add(tempInt);
+					 
+					 sum = sum + tempInt;
+					 
+				 }
+				 
+				 line = br.readLine();
+			
+			 }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("\nNumber of ArrayList elements: " + intAl.size());
+	
+		
 	}
 	
 	
@@ -32,5 +119,17 @@ class Driver {
  * @param args
  */
 class AssignScore {
+	
+	public double calculateAverage(List<Integer> intAl, int sum){
+		
+		return sum/(intAl.size());
+		
+	}
+	
+	void assignScore(){
+		
+	}
+	
+	
 	
 }
