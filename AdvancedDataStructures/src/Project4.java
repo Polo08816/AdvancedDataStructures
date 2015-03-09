b5accebf0d7f955768e1f4df15fe10e23bda9451
Kevin Ku <kkuo12887@gmail.com>
2015-03-08 21:29:59 -0400

    Removed Node class and put into Project4.java

--- AdvancedDataStructures/src/Project4.java
+++ AdvancedDataStructures/src/Project4.java
@@ -41,3 +41,24 @@ public class Project4 {
 	}
 
 }
+
+class Node {
+	
+	String st;
+	Node next;
+	
+	public Node(){
+		
+		this.st = "";
+		this.next = null;
+		
+	}
+	
+	public Node(Node next, String st){
+		
+		this.next = next;
+		this.st = st;
+		
+	}
+	
+}
