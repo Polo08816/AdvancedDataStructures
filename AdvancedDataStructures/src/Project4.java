72d6d8c5a42a950bd913e2381340e31d66da5f35
Kevin Ku <kkuo12887@gmail.com>
2015-03-08 21:15:47 -0400

    Added node and basic functions.

--- AdvancedDataStructures/src/Project4.java
+++ AdvancedDataStructures/src/Project4.java
@@ -15,5 +15,29 @@ public class Project4 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private void insertLine(){
+		
+	}
+	
+	private void deleteLines(int start, int finish){
+		
+	}
+	
+	private void printLines(){
+		
+	}
+	
+	private void printLines(int start, int finish){
+		
+	}
+	
+	private void searchString(String regex){
+		
+	}
+	
+	private void doneExecution(){
+		
+	}
 
 }
