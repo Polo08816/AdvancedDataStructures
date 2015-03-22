c40f78df5f7fa65bbbea815b067811ddcc0c2971
Kevin Ku <kkuo12887@gmail.com>
2015-03-22 00:04:34 -0400

    Added some slides and screenshots.
    Implemented constants.  Replace with enums?

--- AdvancedDataStructures/src/Project4.java
+++ AdvancedDataStructures/src/Project4.java
@@ -6,6 +6,13 @@ import java.io.*;
  *
  */
 public class Project4 {
+	
+	private static final String INSERT_TOKEN = "$insert";
+	private static final String DELETE_TOKEN = "$delete";
+	private static final String PRINT_TOKEN = "$print";
+	private static final String LINE_TOKEN = "$line";
+	private static final String SEARCH_TOKEN = "$search";
+	private static final String DONE_TOKEN = "$done";
 
 	/**
 	 * @param args
@@ -13,18 +20,48 @@ public class Project4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Enter command: ");
+		String command = "";
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			
+			System.out.println("Enter command: ");			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			try {
+				command = br.readLine();
+			} catch (IOException e){
+				System.out.println("IO error trying to read command!");
+				e.printStackTrace();
+			}
+			
+			//tokenize command
+			
+			switch (command){
+				case INSERT_TOKEN: 
+					break;
+				case DELETE_TOKEN:
+					break;
+				case PRINT_TOKEN:
+					break;
+				case LINE_TOKEN:
+					break;
+				case SEARCH_TOKEN:
+					break;
+				case DONE_TOKEN:
+					break;
+				default:
+					System.out.println("Invalid command.  Enter a valid command.");
+					break;
+			
+			}
+			
+		} while (command.equalsIgnoreCase(DONE_TOKEN) == false);
 		
-		String command = null;
 		
-		try {
-			command = br.readLine();
-		} catch (IOException e){
-			System.out.println("IO error trying to read command!");
-			e.printStackTrace();
-		}
+		
+		
+		
+		
 		
 
 	}
@@ -53,6 +90,23 @@ public class Project4 {
 		
 	}
 
+}
+
+enum Command{
+	
+	INSERT_TOKEN("$insert"),
+	DELETE_TOKEN("$delete"),
+	PRINT_TOKEN("$print"),
+	LINE_TOKEN("$line"),
+	SEARCH_TOKEN("$search"),
+	DONE_TOKEN("$done");	
+	
+	private String command_token;
+	
+	private Command(String token){
+		this.command_token = token;
+	}
+	
 }
 
 class Node {
