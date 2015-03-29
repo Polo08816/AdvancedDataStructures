57f08cecd5adf3386a843aa55f15f0e2bf0c5d34
Kevin Ku <kkuo12887@gmail.com>
2015-03-28 21:44:01 -0400

    Working on Driver class.  Established Stack of operators and LinkedList of operands.

--- AdvancedDataStructures/src/OptionalProject1.java
+++ AdvancedDataStructures/src/OptionalProject1.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 /**
  * 
  */
@@ -9,11 +12,51 @@
 public class OptionalProject1 {
 
 	/**
+	 * Creates and executes the Driver class.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Driver dr = new Driver();
 
+	}
+}
+	
+
+class Driver{
+	
+	/**
+	 * Runs the input from the main. 
+	 */
+	
+	private Stack<String> operatorQueue = new Stack<String>();
+	private Queue operandQueue = new LinkedList<String>();
+	
+	public void runDriver(){
+		
+		String expressionEvaluate = "";
+		StringTokenizer st = null;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			
+			while (expressionEvaluate.equalsIgnoreCase("quit") == false){
+				System.out.println("Enter in infix expression OR 'done' if finished");
+				expressionEvaluate = br.readLine();
+			}
+			
+			//process string
+			
+		} catch (NoSuchElementException g){
+			System.out.println("Incorrect command!");
+			g.printStackTrace();
+		} catch (IOException e){
+			System.out.println("IO error trying to read command!");
+			e.printStackTrace();
+		}			
 	}
 
 }
