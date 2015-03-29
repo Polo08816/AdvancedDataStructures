af1c9c771d1a845d0563b84d588e43969b18b2cb
Kevin Ku <kkuo12887@gmail.com>
2015-03-28 23:25:52 -0400

    Implemented some logic for stack and queues.
    To do:
    Complete logic.
    Implement priorities for operator.
    
    Need for 3rd stack?

--- AdvancedDataStructures/src/OptionalProject1.java
+++ AdvancedDataStructures/src/OptionalProject1.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 
@@ -20,6 +22,7 @@ public class OptionalProject1 {
 		// TODO Auto-generated method stub
 		
 		Driver dr = new Driver();
+		dr.runDriver();
 
 	}
 }
@@ -31,13 +34,17 @@ class Driver{
 	 * Runs the input from the main. 
 	 */
 	
-	private Stack<String> operatorQueue = new Stack<String>();
-	private Queue operandQueue = new LinkedList<String>();
+	private Stack<String> operatorStack;
+	private Queue<Double> operandQueue;
 	
 	public void runDriver(){
 		
+		operatorStack = new Stack<String>();
+		operandQueue = new LinkedList<Double>();
+		
 		String expressionEvaluate = "";
 		StringTokenizer st = null;
+		String token = "";
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -46,6 +53,40 @@ class Driver{
 			while (expressionEvaluate.equalsIgnoreCase("quit") == false){
 				System.out.println("Enter in infix expression OR 'done' if finished");
 				expressionEvaluate = br.readLine();
+				st = new StringTokenizer(expressionEvaluate);
+				
+				while (st.hasMoreTokens()){
+					token = st.nextToken().toString();
+					
+					Pattern pattern = Pattern.compile("([0-9]*)(\\.[0-9]*)?");
+					Matcher matcher = pattern.matcher(token);
+					boolean matches = matcher.matches();
+					
+					// is a number - enqueue in operandQueue
+					if (matches){
+						operandQueue.add(Double.parseDouble(token));
+					} else {
+						// if stack is EMPTY push the token onto the stack
+						if (operatorStack.isEmpty()){
+							operatorStack.push(token);
+						// if token equals '(' then push to stack
+						} else if (token.equalsIgnoreCase("(")){
+							operatorStack.push(token);
+						// if token equals ')' pop everything that is not '('
+						} else if (token.equalsIgnoreCase("(")){
+							//check if next on stack is '('
+							while (operatorStack.peek().equalsIgnoreCase("(") == false){
+								//add to operandQueue everything that you pop off operator stack
+								operandQueue.add(Double.parseDouble(operatorStack.pop()));
+							}
+							operatorStack.pop();
+						}
+						
+						
+					}
+				}
+				
+				
 			}
 			
 			//process string
