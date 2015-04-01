0f3b141ae3c67ccab87cb63062c18884cea4cb72
Kevin Ku <kkuo12887@gmail.com>
2015-04-01 18:16:53 -0400

    Project5 files and Javadocs.

---
+++ AdvancedDataStructures/src/Project5.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kevin Kuo
+ *
+ */
+public class Project5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class BinaryTreeOperations{
+	
+	TreeNode seed;
+	
+	public void runBTO(){
+		
+		
+		
+	}
+}
+
+class TreeNode{
+	
+	TreeNode left;
+	TreeNode right;
+	int item;
+	
+	public TreeNode(TreeNode left, int item, TreeNode right){
+		
+		this.left = left;
+		this.right = right;
+		this.item = item;
+		
+	}
+	
+	
+}
