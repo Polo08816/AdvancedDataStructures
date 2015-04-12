b83494bb36c938f49188ffad0bc6f632cea4dd6c
Kevin Ku <kkuo12887@gmail.com>
2015-04-11 21:00:29 -0400

    Implemented QuickSort.

--- AdvancedDataStructures/src/OptionalProject2/OptionalProject2.java
+++ AdvancedDataStructures/src/OptionalProject2/OptionalProject2.java
@@ -43,7 +43,31 @@ public class OptionalProject2 {
 		
 		readFromFile(baseIntegerArray);
 		
-		printIntegerArray(baseIntegerArray);
+//		printIntegerArray(baseIntegerArray);
+		
+		int[] insertionSortArray = new int[20000];
+		int[] quickSortArray = new int[20000];
+		int[] mergeSortArray = new int[20000];
+		int[] heapSortArray = new int[20000];
+		
+		System.arraycopy(baseIntegerArray, 0, insertionSortArray, 0, 19999);
+		System.arraycopy(baseIntegerArray, 0, quickSortArray, 0, 19999);
+		System.arraycopy(baseIntegerArray, 0, mergeSortArray, 0, 19999);
+		System.arraycopy(baseIntegerArray, 0, heapSortArray, 0, 19999);
+		
+//		printIntegerArray(insertionSortArray);
+		insertionSort(insertionSortArray);
+		
+		System.out.println("\nTesting QUICK sort.");
+		long beginTime, endTime;
+		beginTime = System.currentTimeMillis();
+		System.out.println("Begin: " + beginTime);
+		
+		quickSort(quickSortArray, 0, 19999);
+		
+		endTime = System.currentTimeMillis();
+		System.out.println("End: " + endTime);
+		System.out.println("Total: "  + (endTime - beginTime));
 		
 	}
 	
@@ -181,5 +205,66 @@ public class OptionalProject2 {
 			count++;
 		}
 	}
+	
+	private static void insertionSort(int[] inputArray){
+		
+		System.out.println("\nTesting INSERTION sort.");
+		long beginTime, endTime;
+		beginTime = System.currentTimeMillis();
+		System.out.println("Begin: " + beginTime);
+		
+		for (int i = 0; i < inputArray.length; i++){
+			int temp1 = inputArray[i];
+			int temp2;
+			for (temp2 = i - 1; temp2 >= 0 && temp1 < inputArray[temp2]; temp2--){
+				inputArray[temp2 + 1] = inputArray[temp2];
+			}
+			
+			inputArray[temp2 + 1] = temp1;
+		}
+		
+		endTime = System.currentTimeMillis();
+		System.out.println("End: " + endTime);
+		System.out.println("Total: "  + (endTime - beginTime));
+	}
+	
+	private static void quickSort(int[] inputArray, int start, int end){
+		
+		int i = start;
+		int k = end;
+		
+		if (end - start >= 1){
+			
+			int pivot = inputArray[start];       // set the pivot as the first element in the partition
+
+            while (k > i)                   // while the scan indices from left and right have not met,
+            {
+                    while (inputArray[i] <= pivot && i <= end && k > i)  // from the left, look for the first
+                            i++;                                    // element greater than the pivot
+                    while (inputArray[k] > pivot && k >= start && k >= i) // from the right, look for the first
+                        k--;                                        // element not greater than the pivot
+                    if (k > i)                                       // if the left seekindex is still smaller than
+                            swap(inputArray, i, k);                      // the right index, swap the corresponding elements
+            }
+            swap(inputArray, start, k);          // after the indices have crossed, swap the last element in
+                                            // the left partition with the pivot 
+            quickSort(inputArray, start, k - 1); // quicksort the left partition
+            quickSort(inputArray, k + 1, end);   // quicksort the right partition
+            
+            
+			
+		} else {
+			return;
+		}
+		
+	}
+	
+	private static void swap (int array[], int index1, int index2){
+		int temp = array[index1];           // store the first value in a temp
+		array[index1] = array[index2];      // copy the value of the second into the first
+		array[index2] = temp;
+	}
+	
+	
 
 }
