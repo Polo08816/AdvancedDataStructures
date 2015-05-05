985775199b4519f31c03f2862737afe4034f7ef9
Kevin Ku <kkuo12887@gmail.com>
2015-05-04 21:48:33 -0400

    Adding input file and some changes.

--- AdvancedDataStructures/src/Project6.java
+++ AdvancedDataStructures/src/Project6.java
@@ -12,12 +12,14 @@ public class Project6 {
 		ArrayList<String> statesArray = new ArrayList<String>();
 		statesArray = dr.readFileStates(statesArray, "project6_inputState.txt");
 		
-//		dr.printArrayList(statesArray);		
+		dr.printArrayList(statesArray);		
 		
 		Graph stateGraph = new Graph();		
 		stateGraph.populateGraphArraywithStateArray(statesArray);
 		
+		dr.readFileAdjacentStates(stateGraph.graphNodeArray, "project6_inputAdjacency.txt");
 		
+		dr.printGraphArrayList(stateGraph.graphNodeArray);
 
 	}
 
@@ -30,12 +32,14 @@ class Graph{
 	public Graph(){
 		
 		this.graphNodeArray = new ArrayList<GraphNode>();
+		this.graphNodeArray.add(new GraphNode(-1));
 		
 	}
 	
 	public void addGraphNode(GraphNode graphNode){
 		
 		this.graphNodeArray.add(graphNode);
+		
 		
 	}
 	
@@ -62,6 +66,11 @@ class Graph{
 	public void populateGraphArraywithStateArray(ArrayList<String> inputArray){
 				
 		Iterator<String> it = inputArray.iterator();
+		if (it.hasNext()){
+			it.next();
+		} else {
+			return;
+		}	
 		
 		while (it.hasNext()){
 //			System.out.println(it.next().toString());
@@ -121,6 +130,23 @@ class GraphNode{
 	private LinkedList<Integer> getAdjacentStates() {
 		return adjacentStates;
 	}
+	
+	/**
+	 * @param adjacentStates the adjacentStates to add
+	 */
+	public void addAdjacentStates(int stateInt) {
+		this.adjacentStates.add(stateInt);
+	}
+	
+	private int searchAdjacentState(int stateInt){
+		Iterator<Integer> it = this.adjacentStates.iterator();
+		return 1;
+	}
+
+	public String toString(){		
+		
+		return "State number: " + this.nodeValue + " \nLinkedList size: " + this.adjacentStates.size() + "\nLinkedList contents: " + this.adjacentStates.toString() + " \n";
+	}
 
 }
 
@@ -151,6 +177,7 @@ class Driver{
 					try {
 						
 						tempLine = tempLine + " " + st.nextToken();
+						tempLine = tempLine.trim();
 						
 					} catch (NumberFormatException h){
 //						h.printStackTrace();
@@ -177,6 +204,56 @@ class Driver{
 		return statesArray;		
 	}
 	
+public ArrayList<GraphNode> readFileAdjacentStates(ArrayList<GraphNode> graphNodeArrayList, String fileName){
+		
+		BufferedReader br;
+		String line, tempLine;
+		StringTokenizer st;
+		
+		try {
+				
+			System.out.println("\nCurrent working directory: " + System.getProperty("user.dir") + File.separator + fileName);
+			br = new BufferedReader(new FileReader(System.getProperty("user.dir") + File.separator + fileName));
+
+			line = br.readLine();
+			
+			int index = 1;
+			while (line != null){
+				tempLine = "";
+				st = new StringTokenizer(line);
+				
+				while (st.hasMoreTokens()){
+					int tempInt = -1;
+					try {						
+						
+						tempInt = Integer.parseInt(st.nextToken());
+						
+//						System.out.println("State added to LL: " + tempInt);
+						
+						graphNodeArrayList.get(index).addAdjacentStates(tempInt);
+						
+					} catch (NumberFormatException h){
+//						h.printStackTrace();
+					}					
+				}
+				index++;
+				line = br.readLine();
+			}	
+			br.close();
+		} catch (FileNotFoundException f){
+			System.out.println("\n" + fileName + " was not found in the current working directory.\n");
+			f.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error tokenizing the input stream.");
+			e.printStackTrace();
+		}
+		
+		System.out.println("\nGraph ArrayList size: " + graphNodeArrayList.size());
+		
+		return graphNodeArrayList;		
+	}
+	
 	/**
 	 * Prints values in ArrayList.
 	 * 
@@ -184,9 +261,25 @@ class Driver{
 	 */
 	public void printArrayList(ArrayList<String> inputArray){
 		
-		System.out.println("\nPrinting contents of ArrayList:\n");
+		System.out.println("\nPrinting contents of States ArrayList:\n");
 		
 		Iterator<String> it = inputArray.iterator();
+		while (it.hasNext()){
+			System.out.println(it.next().toString());			
+		}
+	}
+	
+	
+	/**
+	 * Prints integer (state) in ArrayList.
+	 * 
+	 * @param inputArray ArrayList of String
+	 */
+	public void printGraphArrayList(ArrayList<GraphNode> inputArray){
+		
+		System.out.println("\nPrinting contents of Graph ArrayList:\n");
+		
+		Iterator<GraphNode> it = inputArray.iterator();
 		while (it.hasNext()){
 			System.out.println(it.next().toString());			
 		}
